f77ade3daaf8688bf6bd856fdaf01f0dd49ab488
Patricia Correia <pncorreia25@gmail.com>
2022-04-07 14:03:33 -0300

    adiciona mais questões no exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,10 +1,47 @@
+// Questão 1 do exercício
 const a = 15;
 const b = 4;
+
 console.log(a + b);
 console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 console.log(a % b);
+
+// Questão 2 
 const numb1 = 3; 
 const numb2 = 5;
-console.log(numb1 <= numb2);
+
+if (numb1 >= numb2){
+    console.log(numb1)
+}else{
+    console.log(numb2)
+}
+
+// Questão 3 
+const valor1 = 10;
+const valor2 = 3;
+const valor3 = 7;
+
+if (valor1 > valor2 && valor3){
+    console.log(valor1)
+}else if(valor2 > valor1 && valor3){
+    console.log(valor2)
+}else{
+    console.log(valor3)
+}
+
+// Questão 4 
+const value = -4;
+
+if (value > 0){
+    console.log('positivo')
+}else if (value < 0){
+    console.log('negativo')
+}else{
+    console.log('zero')
+}
+
+// Questão 5 
+// Questão 6
+// Questão 7
